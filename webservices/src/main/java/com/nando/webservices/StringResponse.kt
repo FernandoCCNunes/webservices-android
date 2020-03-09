b927220224208249c89a10ef289fb6b1d3e0308f
Fernando Nunes <fernandocruznunes@enta.pt>
2020-03-09 13:44:43 -0100

    Save Project

--- webservices/src/main/java/com/nando/webservices/StringResponse.kt
+++ webservices/src/main/java/com/nando/webservices/StringResponse.kt
@@ -1,4 +1,4 @@
 package com.nando.webservices
 
-data class StringResponse(val data: String?, val statusCode: Int?, val networkTimeMs: Long?, val isSuccessful: Boolean = false, val cause: Throwable?, val message: String?, val localizedMessage: String?)
+data class StringResponse(val data: String?, val statusCode: Int?, val networkTimeMs: Long?, val isSuccessful: Boolean, val cause: Throwable?, val message: String?, val localizedMessage: String?)
     
